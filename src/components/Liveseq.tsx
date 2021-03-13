cbc36dfead43b0a142d291f9e30fbc097503b557
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-03-13 15:56:20 -0300

    remove outdated comment

--- src/components/Liveseq.tsx
+++ src/components/Liveseq.tsx
@@ -4,8 +4,6 @@ import type { LiveseqProps } from '../liveseq';
 import { PlaybackButton } from './PlaybackButton';
 
 export const Liveseq: React.FunctionComponent<LiveseqProps> = (props) => {
-  // Note that LiveseqProvider is a WIP implementation
-  // eventually will allow us to use hooks like useIsPlaying
   const { LiveseqProvider } = useLiveseq(props);
 
   return (
