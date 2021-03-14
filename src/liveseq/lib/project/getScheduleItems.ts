0ab681c30702f1bca695c025dc146e39afcdc0fc
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-03-13 23:41:43 -0300

    use reduce instead of map and filter

--- src/liveseq/lib/project/getScheduleItems.ts
+++ src/liveseq/lib/project/getScheduleItems.ts
@@ -19,41 +19,34 @@ export const getScheduleItems = (
     const slot = entities.slots[slotId];
     const timeline = entities.timelines[slot.timelineId];
     const timelineClips = getTimelineClips(timeline, entities.clips);
+    const channels = getChannelsBySlotId(entities.channels, slot.id);
 
-    const notesWithChannels = getChannelsBySlotId(entities.channels, slot.id)
-      .map((channel) => {
-        return {
-          notes: getTimelineNotesInRange(
-            beatsRange,
-            timeline,
-            timelineClips,
-            channel.id,
-            slot.id,
-            slot.loops,
-          ),
-          channel,
-        };
-      })
-      .filter(({ notes }) => {
-        // remove the ones that have no notes
-        return notes.length > 0;
+    return channels.reduce((accumulator, channel) => {
+      const notes = getTimelineNotesInRange(
+        beatsRange,
+        timeline,
+        timelineClips,
+        channel.id,
+        slot.id,
+        slot.loops,
+      );
+
+      if (notes.length === 0) return accumulator;
+
+      const instrument = entities.instruments[channel.instrumentId];
+
+      accumulator.push({
+        instrument,
+        notes: notes.map((note) => {
+          return {
+            ...note,
+            startTime: beatsToTime(note.start, bpm),
+            endTime: beatsToTime(note.end, bpm),
+          };
+        }),
       });
 
-    return notesWithChannels.map(
-      (notesWithChannel): ScheduleItem => {
-        const instrument = entities.instruments[notesWithChannel.channel.instrumentId];
-
-        return {
-          instrument,
-          notes: notesWithChannel.notes.map((note) => {
-            return {
-              ...note,
-              startTime: beatsToTime(note.start, bpm),
-              endTime: beatsToTime(note.end, bpm),
-            };
-          }),
-        };
-      },
-    );
+      return accumulator;
+    }, [] as Array<ScheduleItem>);
   });
 };
