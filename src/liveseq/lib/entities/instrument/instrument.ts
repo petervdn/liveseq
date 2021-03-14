1213daf16c5c6d0fe9db78e3fd104baa68848523
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-03-13 21:00:41 -0300

    move type

--- src/liveseq/lib/entities/instrument/instrument.ts
+++ src/liveseq/lib/entities/instrument/instrument.ts
@@ -1,8 +1,13 @@
 import type { LiveseqEntity } from '../liveseqEntity';
 import type { SamplerInstrument } from './sampler';
+import type { ScheduleNote } from '../../player/player';
 
 export type SimpleSynthInstrument = LiveseqEntity & {
   type: 'simpleSynthInstrument';
 };
 
 export type Instrument = SamplerInstrument | SimpleSynthInstrument;
+
+export type InstrumentInstance = {
+  schedule: (context: AudioContext, notes: Array<ScheduleNote>) => void;
+};
--- src/liveseq/lib/entities/instrument/sampler.ts
+++ src/liveseq/lib/entities/instrument/sampler.ts
@@ -2,19 +2,17 @@ import type { ScheduleNote } from '../../player/player';
 import { playTick } from '../../utils/playTick';
 import type { LiveseqEntity } from '../liveseqEntity';
 import { getFrequency } from '../../note/note';
+import type { InstrumentInstance } from './instrument';
 
 export type SamplerInstrument = LiveseqEntity & {
   type: 'samplerInstrument';
-};
-type Instrument = {
-  schedule: (context: AudioContext, notes: Array<ScheduleNote>) => void;
 };
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type SamplerProps = {};
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const createSampler = (props: SamplerProps): Instrument => {
+export const createSampler = (props: SamplerProps): InstrumentInstance => {
   return {
     schedule: (context: AudioContext, notes: Array<ScheduleNote>) => {
       notes.forEach((note) => {
