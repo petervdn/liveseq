1e473098c8105b4d7ccceea3c68d1744f899c18e
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-03-27 16:29:29 -0300

    add general liveseq init test

--- src/liveseq/lib/utils/getAudioContext.ts
+++ src/liveseq/lib/utils/getAudioContext.ts
@@ -1,4 +1,15 @@
-export const getAudioContext = () => {
-  /* eslint-disable @typescript-eslint/no-explicit-any */
-  return new (window.AudioContext || (window as any).webkitAudioContext)();
+export const getAudioContext = (): AudioContext => {
+  // this is split in 2 ifs like this because it was throwing error in nodejs
+  if ('AudioContext' in window) {
+    return new window.AudioContext();
+  }
+
+  if ('webkitAudioContext' in window) {
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+    // eslint-disable-next-line new-cap
+    return new (window as any).webkitAudioContext();
+  }
+
+  // this is for nodejs (jest testing).
+  return {} as AudioContext;
 };
