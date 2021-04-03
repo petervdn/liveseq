d25d02c2679372df018670f52504b408302bb105
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-04-03 18:20:55 -0300

    add tempo input

---
+++ src/components/Tempo.tsx
@@ -0,0 +1,18 @@
+import { useTempo } from '../liveseq/react/useTempo';
+
+export const Tempo = () => {
+  const [tempo, setTempo] = useTempo();
+
+  return (
+    <>
+      tempo:
+      <input
+        type="number"
+        value={tempo}
+        onChange={(changeEvent) => {
+          setTempo(parseInt(changeEvent.target.value, 10));
+        }}
+      />
+    </>
+  );
+};
---
+++ src/liveseq/react/useTempo.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+import { useLiveseqContext } from './useLiveseq';
+import type { Bpm } from '..';
+
+export const useTempo = () => {
+  const liveseq = useLiveseqContext();
+  const [tempo, setTempoInternal] = useState(liveseq.getTempo);
+
+  useEffect(() => {
+    return liveseq.subscribe.tempoChange((tempo) => setTempoInternal(tempo));
+  }, [liveseq]);
+
+  const setTempo = (tempo: number) => {
+    liveseq.setTempo(tempo as Bpm);
+  };
+
+  return [tempo, setTempo] as const;
+};
