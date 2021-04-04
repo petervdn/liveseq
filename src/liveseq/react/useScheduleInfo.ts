3c04f6ea8651a9da388991109e5cfbe8347f85f2
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-04-03 21:54:08 -0300

    Update useScheduleInfo.ts

--- src/liveseq/react/useScheduleInfo.ts
+++ src/liveseq/react/useScheduleInfo.ts
@@ -1,16 +1,19 @@
 import { useEffect, useState } from 'react';
 import { useLiveseqContext } from './useLiveseq';
 import type { BeatsRange } from '..';
+import { createRange } from '../lib/time/beatsRange';
 
 export const useScheduleInfo = (start: number, end: number) => {
   const liveseq = useLiveseqContext();
-  const [info, setInfo] = useState(liveseq.getScheduleItemsInfo({ start, end } as BeatsRange));
+  const [info, setInfo] = useState(liveseq.getScheduleItemsInfo(createRange(start, end)));
 
   useEffect(() => {
-    return liveseq.onSchedule((data) =>
+    return liveseq.onSchedule((data) => {
       // TODO: fix
-      setInfo(liveseq.getScheduleItemsInfo((data.nextSlotPlaybackState as unknown) as BeatsRange)),
-    );
+      return setInfo(
+        liveseq.getScheduleItemsInfo((data.nextSlotPlaybackState as unknown) as BeatsRange),
+      );
+    });
   }, [liveseq]);
 
   return info;
