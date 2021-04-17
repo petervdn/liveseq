e979a5f013f1e2eed06414f1626fbfa7ade7c052
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-04-17 19:11:16 -0300

    move projects away from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,13 @@
 import { Liveseq } from './components/Liveseq';
-import { Beats, createLiveseq, getAbSwitch } from './liveseq';
-import { addCompleteRouting } from './components/utils/addCompletRouting';
-import { getMetronomeNotes } from './liveseq/projects/abSwitch';
+import { getAbSwitch } from './liveseq';
 import { GlobalStyle } from './components/general/GlobalStyle';
 import { Tabs } from './components/general/Tabs';
-import { times } from './liveseq/lib/utils/times';
+import { getSimpleMetronome } from './liveseq/projects/simpleMetronome';
 
-const liveseq = createLiveseq({ project: { name: 'Test project' } });
-
-const offset = 4;
-times(1, (index) => {
-  addCompleteRouting({
-    liveseq,
-    notes: getMetronomeNotes(index % 2 === 1),
-    sceneStart: (offset * index) as Beats,
-    name: `Routing ${index}`,
-  });
-});
-
-const simpleMetronome = liveseq.getProject();
-
-const projects = [
-  {
-    label: 'Simple Metronome',
-    project: simpleMetronome,
-  },
-  {
-    label: 'A/B Switch',
-    project: getAbSwitch(),
-  },
-];
+const projects = [getSimpleMetronome(), getAbSwitch()].map((project) => ({
+  label: project.name,
+  project,
+}));
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function App() {
